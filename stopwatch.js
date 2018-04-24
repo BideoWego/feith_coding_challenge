35355c77d65b4e95c26505da718b35e2d7259534
Bideo Wego <bideowego@gmail.com>
2018-04-24 17:22:20 -0400

    Bugfix

--- stopwatch.js
+++ stopwatch.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const interval = 10;
   let startTime;
   let diff;
+  let lastDiff = 0;
 
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
@@ -14,33 +15,43 @@ document.addEventListener('DOMContentLoaded', function() {
   const seconds = document.getElementById('seconds');
   const milliseconds = document.getElementById('milliseconds');
 
-  start.addEventListener('click', function(e) {
-    if (isRunning) {
-      return;
-    }
 
-    if (!startTime) {
-      startTime = new Date().getTime();
-    }
-    isRunning = true;
-
+  const startInterval = () => {
     id = setInterval(function() {
-      diff = new Date().getTime() - startTime;
+      diff = new Date().getTime() - startTime + lastDiff;
       let secondsNum = Math.floor(diff / 1000);
       let millisecondsNum = Math.floor(diff % 1000 / 10);
       seconds.innerHTML = secondsNum;
       milliseconds.innerHTML = millisecondsNum.toString().padStart(2, '0');
     }, interval);
+  };
+
+  const stopInterval = () => {
+    isRunning = false;
+    lastDiff = new Date().getTime() - startTime + lastDiff;
+    clearInterval(id);
+  };
+
+
+  start.addEventListener('click', function(e) {
+    if (isRunning) {
+      return;
+    }
+
+    startTime = new Date().getTime();
+    isRunning = true;
+
+    startInterval();
   });
 
   stop.addEventListener('click', function(e) {
-    isRunning = false;
-    diff = new Date().getTime() - startTime;
-    clearInterval(id);
+    stopInterval();
   });
 
   reset.addEventListener('click', function(e) {
-    startTime = null;
+    startTime = new Date().getTime();
+    diff = 0;
+    lastDiff = 0;
     seconds.innerHTML = 0;
     milliseconds.innerHTML = '00';
   });
